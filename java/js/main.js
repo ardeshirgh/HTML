6ce1f42873d41038d5f04b21e61da814e3ee8f47
ardeshirgh <52971106+ardeshirgh@users.noreply.github.com>
2019-08-12 12:29:27 -0700

    Add files via upload

---
+++ java/js/main.js
@@ -0,0 +1,25 @@
+
+
+var date = new Date();
+
+// GET ELEMENT BY ID EXAMPLE
+document.getElementById('fromJS').innerHTML= date; 
+
+
+
+
+
+// GET ELEMENT BY ID EXAMPLE
+
+
+
+// ALERT EXAMPLE
+alert("now I have JS");
+
+
+
+// GET ELEMENT BY CLASS EXAMPLE
+document.getElementsByClassName('myClass')[0].innerHTML="Class 1";
+document.getElementsByClassName('myClass')[1].innerHTML="Class 2";
+document.getElementsByClassName('myClass')[2].innerHTML="These are classes";
+
